ce7ff6c1f95aa3643859de99fd7b543cbadc0717
Saurabh <ss0274307@gmail.com>
2024-01-27 22:36:02 +0530

    Add real data and links for intro page

--- src/PortFolioIntro.js
+++ src/PortFolioIntro.js
@@ -4,14 +4,16 @@ import { Flex } from "@react-spectrum/layout";
 const PortFolioIntro = () => (
     <Flex direction="column" justifyContent="center" alignItems="center" flex="1">
         <Heading level={2} UNSAFE_className={ PortfolioIntroCss.heading }>
-            Hi, my name is Shriya and I use data science and 
-            causal inference methods to tackle public health 
-            challenges.
+            Hey, this is Shriya Grover, a researcher in biology using 
+            electromicrobiology to tackle problems in the field of 
+            sustainable energy production. 
         </Heading>
         <Text UNSAFE_className={ PortfolioIntroCss.text }>
-            I'm a PhD candidate at Boston University School of Public Health.
-            My dissertation uses machine learning and causal inference methods
-            to predict and explain suicide-related outcomes among U.S. Veterans.
+            I'm a PhD candidate at the Department of 
+            Biochemical Engineering and Biotechnology, 
+            where my dissertation involves enhancing electron 
+            transfer between electroactive microorganisms 
+            and electrodes.
         </Text>
     </Flex>
 );
