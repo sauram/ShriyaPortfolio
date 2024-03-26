c86b616c101a7acd33558a412562a713dcf8a1ea
Saurabh <ss0274307@gmail.com>
2024-03-26 14:14:12 +0530

    Fix publication font size

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,4 +1,5 @@
 import { Heading, Text } from '@react-spectrum/text';
+import { Link } from '@react-spectrum/link';
 import PublicationCss from './Publications.module.css'
 const Publications = ({isActive}) => (
     <div className={`${PublicationCss.container} ${isActive ? PublicationCss.active : ''}`}>
@@ -6,7 +7,7 @@ const Publications = ({isActive}) => (
             Working papers and papers under review
         </Heading>
         <Text UNSAFE_className= { PublicationCss.text }>
-            1. <a rel='noreferrer' href="https://doi.org/10.1016/j.trechm.2024.01.005" target='_blank'><span>Grover, S.</span> and Doyle, L.E. Advanced electrode materials for microbial extracellular electron transfer. Trends in Chemistry <span>(Cell Press) (In Press) [Impact factor: 15.7]</span></a>
+            1. <span>Grover, S.</span> and Doyle, L.E. Advanced electrode materials for microbial extracellular electron transfer. Trends in Chemistry <span>(Cell Press) (In Press) [Impact factor: 15.7]</span> <Link rel='noreferrer' href="https://doi.org/10.1016/j.trechm.2024.01.005" target='_blank'>(Click here to read)</Link>
         </Text>
         <Text UNSAFE_className= { PublicationCss.text }>
             2. <span>Grover, S.</span> Yedla, A. Sarkar, S. and Doyle, L.E. Conductive polymer electrode integration to amplify current output of Shewanella oneidensis.(in preparation)
